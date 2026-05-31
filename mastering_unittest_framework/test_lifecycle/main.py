7d4d556411d46bd1bfbe79c73db2a38252020dd4
TyreseJORDANEBLAIR <>
2026-05-31 23:46:20 +0000

    Test Lifecycle

---
+++ mastering_unittest_framework/test_lifecycle/main.py
@@ -0,0 +1,34 @@
+import unittest
+
+class Book:
+    def __init__(self, title, author, amount):
+        self.title = title
+        self.author = author
+        self.amount = amount
+
+    def __repr__(self):
+        return f"<Book {self.title}, written by {self.author}, {self.amount} pieces>"
+
+    def sale(self):
+        if self.amount > 0:
+            self.amount -= 1
+        else:
+            return "This book sold out"
+
+# Write your code below:
+class TestBook(unittest.TestCase):
+    def setUp(self):
+        self.book = Book("1984", "George Orwell", 2)
+
+    def tearDown(self):
+        del self.book
+
+    def test_sale(self):
+        self.book.sale()
+        result = self.book.sale()
+        self.assertEqual(result, "This book sold out")
+
+    def test_book_repr(self):
+        self.assertEqual(repr(self.book),
+                        "<Book 1984, written by George Orwell, 2 pieces>"
+                        )
